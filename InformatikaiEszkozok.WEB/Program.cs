b332e72d6133891542b4c66ee1e25091213fd76d
József Méhes <mehesj@outlook.hu>
2026-05-16 20:25:34 +0200

    KosarService

--- InformatikaiEszkozok.WEB/Program.cs
+++ InformatikaiEszkozok.WEB/Program.cs
@@ -1,10 +1,13 @@
 using InformatikaiEszkozok.WEB.Components;
+using InformatikaiEszkozok.WEB.SERVICE;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddScoped<KosarService>();
 
 var app = builder.Build();
 
---
+++ InformatikaiEszkozok.WEB/SERVICE/KosarService.cs
@@ -0,0 +1,16 @@
+﻿using InformatikaiEszkozok.WEB.ViewModel;
+
+namespace InformatikaiEszkozok.WEB.SERVICE
+{
+    public class KosarService
+    {
+        public List<KosarTetel> Tetelek { get; set; } = new();
+
+        public int Darabszam => Tetelek.Sum(t => t.Mennyiseg);
+
+        public void Urit()
+        {
+            Tetelek.Clear();
+        }
+    }
+}
