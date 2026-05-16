42e2a01759e8eabe277e24dc05db8a6c5295b36b
József Méhes <mehesj@outlook.hu>
2026-05-16 19:13:02 +0200

    Kosárba helyezés

---
+++ InformatikaiEszkozok.WEB/ViewModel/KosarTetel.cs
@@ -0,0 +1,11 @@
+﻿namespace InformatikaiEszkozok.WEB.ViewModel
+{
+    public class KosarTetel
+    {
+        public int TermekId { get; set; }
+        public string? Nev { get; set; }
+        public int Egysegar { get; set; }
+        public int Mennyiseg { get; set; } = 0;
+        public string? KepFajlnev { get; set; }
+    }
+}
